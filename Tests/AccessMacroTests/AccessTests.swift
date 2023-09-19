36480488be869f2d612dde0570f69c210598821f
Roman Abuziarov <roman@ultimate.app>
2023-09-19 12:07:43 +0200

    create tests

--- Tests/AccessMacroTests/AccessTests.swift
+++ Tests/AccessMacroTests/AccessTests.swift
@@ -1,26 +1,32 @@
-#if canImport(AccessMacros)
+#if canImport(AccessMacroImplementation)
 import SwiftSyntaxMacroExpansion
 import SwiftSyntaxMacros
 import SwiftSyntax
-import AccessMacros
+import AccessMacroImplementation
 import SwiftSyntaxMacrosTestSupport
 import XCTest
-
+import MacroTesting
 
 final class ComposableActionTests: XCTestCase {
-    private let macros: [String: Macro.Type] = [
-        "Access": AccessMacro.self
-    ]
+    override func invokeTest() {
+        withMacroTesting(
+            macros: [
+                "Access": AccessMacro.self
+            ],
+            operation: super.invokeTest
+        )
+    }
 
     func testSimple() {
-        assertMacroExpansion(
+        assertMacro {
             """
             enum Action {
                 @Access
                 struct A {}
             }
-            """,
-            expandedSource: """
+            """
+        } matches: {
+            """
             enum Action {
                 struct A {}
 
@@ -31,13 +37,12 @@ final class ComposableActionTests: XCTestCase {
                     }
                 }
             }
-            """,
-            macros: macros
-        )
+            """
+        }
     }
 
     func testPublicEnumFileprivateEmit() {
-        assertMacroExpansion(
+        assertMacro {
             """
             enum Action {
                 @Access(emit: .fileprivate)
@@ -45,27 +50,31 @@ final class ComposableActionTests: XCTestCase {
                     case action
                 }
             }
-            """,
-            expandedSource: """
+            """
+        } matches: {
+            """
             enum Action {
-                public enum Delegate {
+                public enum Delegate: Equatable {
                     case action
                 }
 
-                public struct DelegateAccessor {
+                public struct DelegateAccessor: Equatable {
                     public let value: Delegate
                     fileprivate init(_ value: Delegate) {
                         self.value = value
                     }
+                    fileprivate static let action = Self (.action)
+                    public func `is`(_ value: Delegate) -> Bool {
+                        self.value == value
+                    }
                 }
             }
-            """,
-            macros: macros
-        )
+            """
+        }
     }
 
     func testFileprivateEnumEmit() {
-        assertMacroExpansion(
+        assertMacro {
             """
             enum Action {
                 @Access(emit: .fileprivate)
@@ -73,8 +82,9 @@ final class ComposableActionTests: XCTestCase {
                     case action
                 }
             }
-            """,
-            expandedSource: """
+            """
+        } matches: {
+            """
             enum Action {
                 fileprivate enum Private {
                     case action
@@ -85,15 +95,15 @@ final class ComposableActionTests: XCTestCase {
                     fileprivate init(_ value: Private) {
                         self.value = value
                     }
+                    fileprivate static let action = Self (.action)
                 }
             }
-            """,
-            macros: macros
-        )
+            """
+        }
     }
 
     func testFileprivateAccessPublicEmit() {
-        assertMacroExpansion(
+        assertMacro {
             """
             enum Action {
                 @Access(read: .fileprivate, emit: .public)
@@ -101,8 +111,9 @@ final class ComposableActionTests: XCTestCase {
                     case action
                 }
             }
-            """,
-            expandedSource: """
+            """
+        } matches: {
+            """
             enum Action {
                 public enum Public {
                     case action
@@ -113,11 +124,113 @@ final class ComposableActionTests: XCTestCase {
                     public init(_ value: Public) {
                         self.value = value
                     }
+                    public static let action = Self (.action)
                 }
             }
-            """,
-            macros: macros
-        )
+            """
+        }
+    }
+
+    func testComplexAction() {
+        assertMacro {
+            """
+            public enum Action {
+                @Access(read: .fileprivate)
+                public enum Public: Equatable {
+                    case a(Int)
+                }
+
+                @Access(emit: .fileprivate)
+                public enum Delegate {
+                    case b(value: Int)
+                }
+
+                @Access
+                fileprivate enum Fileprivate {
+                    case c(_ value: Int)
+                }
+
+                @Access
+                enum Internal {
+                    case d(Int), e(Int), f
+                }
+
+                case `public`(PublicAccessor)
+                case delegate(DelegateAccessor)
+                case `fileprivate`(FileprivateAccessor)
+                case `internal`(InternalAccessor)
+            }
+            """
+        } matches: {
+            """
+            public enum Action {
+                public enum Public: Equatable {
+                    case a(Int)
+                }
+
+                public struct PublicAccessor: Equatable {
+                    fileprivate let value: Public
+                    public init(_ value: Public) {
+                        self.value = value
+                    }
+                    public static func a(_ param0: Int) -> Self {
+                        return Self (.a(_: param0))
+                    }
+                    fileprivate func `is`(_ value: Public) -> Bool {
+                        self.value == value
+                    }
+                }
+                public enum Delegate {
+                    case b(value: Int)
+                }
+
+                public struct DelegateAccessor {
+                    public let value: Delegate
+                    fileprivate init(_ value: Delegate) {
+                        self.value = value
+                    }
+                    fileprivate static func b(value: Int) -> Self {
+                        return Self (.b(value: value))
+                    }
+                }
+                fileprivate enum Fileprivate {
+                    case c(_ value: Int)
+                }
+
+                public struct FileprivateAccessor {
+                    fileprivate let value: Fileprivate
+                    fileprivate init(_ value: Fileprivate) {
+                        self.value = value
+                    }
+                    fileprivate static func c(_  value: Int) -> Self {
+                        return Self (.c(_ : value))
+                    }
+                }
+                enum Internal {
+                    case d(Int), e(Int), f
+                }
+
+                public struct InternalAccessor {
+                    let value: Internal
+                    init(_ value: Internal) {
+                        self.value = value
+                    }
+                    static func d(_ param0: Int) -> Self {
+                        return Self (.d(_: param0))
+                    }
+                    static func e(_ param0: Int) -> Self {
+                        return Self (.e(_: param0))
+                    }
+                    static let f = Self (.f)
+                }
+
+                case `public`(PublicAccessor)
+                case delegate(DelegateAccessor)
+                case `fileprivate`(FileprivateAccessor)
+                case `internal`(InternalAccessor)
+            }
+            """
+        }
     }
 }
 #endif
